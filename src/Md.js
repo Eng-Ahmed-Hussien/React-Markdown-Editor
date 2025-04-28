40c20a4c70667d4b0db522dbcce1bae694587a14
Eng-Ahmed-Hussien <Ahmed.hussien685@el-eng.menofia.edu.eg>
2025-04-28 04:34:17 +0300

    Upload Project files......

---
+++ src/Md.js
@@ -0,0 +1,24 @@
+import ReactMarkdown from 'react-markdown';
+import { useState } from 'react';
+import './Md.css';
+
+function Md() {
+
+    const [markdown, setMarkdown] = useState("");
+
+    return (
+        <>
+            <div className="container">
+                <h1 className='heading'>React Markdown Editor</h1>
+                <div className="flex">
+                    <textarea type="text" value={markdown} onChange={(e) => setMarkdown(e.target.value)} />
+                    <div className="input-field">
+                        <ReactMarkdown>{markdown}</ReactMarkdown>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Md;
